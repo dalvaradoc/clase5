b3fefb03746596ebaf9fd2a34acaa8337358c77c
Estudiante <Estudiante@10.203.1.109>
2018-08-27 07:36:44 -0500

    2

---
+++ src/javaapplication5/Cuenta.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication5;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Cuenta {
+    public int numero;
+    public int clave;
+    public String dueno;
+    public double saldo;
+    
+    
+}
