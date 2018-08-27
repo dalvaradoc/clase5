b06e47a031888f5d44c637adbc92f2642a079348
Estudiante <Estudiante@10.203.1.109>
2018-08-27 08:24:50 -0500

    2

--- src/javaapplication5/Cuenta.java
+++ src/javaapplication5/Cuenta.java
@@ -10,10 +10,74 @@ package javaapplication5;
  * @author Estudiante
  */
 public class Cuenta {
-    public int numero;
-    public int clave;
-    public String dueno;
-    public double saldo;
+    
+    private int numero;
+    private int clave;
+    private String dueno;
+    private double saldo;
+    
+    public static int global;
+    
+    public Cuenta (int numero, int clave, String dueno, double saldo){
+        this.numero = numero;
+        this.clave = clave;
+        this.dueno = dueno;
+        this.saldo = saldo;
+    }
     
     
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public int getClave() {
+        return clave;
+    }
+
+    public String getDueno() {
+        return dueno;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public void setClave(int clave) {
+        this.clave = clave;
+    }
+
+    public void setDueno(String dueno) {
+        this.dueno = dueno;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+    
+    public void consignar (double valor){
+        this.saldo += valor;
+    }
+    
+    public boolean retirar (double valor){
+        if (valor > 0 && valor <= this.saldo){
+            this.saldo -= valor;
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    public boolean cambiarClave (int clave){
+        setClave(clave);
+        return true;
+    }
+    
+    public double consultarSaldo (){
+        return saldo;
+    }
 }
