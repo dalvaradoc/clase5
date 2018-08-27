1b2a89caeee5b3acfb09e5f6481e64af3966bab5
Estudiante <Estudiante@10.203.1.109>
2018-08-27 08:56:11 -0500

    2

--- src/javaapplication5/Main.java
+++ src/javaapplication5/Main.java
@@ -5,25 +5,41 @@
  */
 package javaapplication5;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Estudiante
  */
 public class Main {
     public static void main(String[] args){
-        Cuenta cuenta = new Cuenta(1, 123, "f", 100);
+//        Cuenta cuenta = new Cuenta(1, 123, "f", 100);
+//        
+//        Cuenta.global = 50;
+//        
+//        System.out.println("Global: " + Cuenta.global + '\n');
+//        
+//        System.out.println("Numero: " + cuenta.getNumero());
+//        System.out.println("Clave: " + cuenta.getClave());
+//        System.out.println("Dueno: " + cuenta.getDueno());
+//        System.out.println("Saldo: " + cuenta.getSaldo());
+//        
+//        cuenta.setClave(3484);
+//        
+//        System.out.println("Clave: " + cuenta.getClave());
+
+        Scanner lectura = new Scanner(System.in);
         
-        Cuenta.global = 50;
+        System.out.println("Ingrese el numero de la cuenta");
+        int cuenta = lectura.nextInt();
+        System.out.println("Ingrese la clave");
+        int clave = lectura.nextInt();
+        System.out.println("Ingrese dueno");
+        String dueno = lectura.next();
+        System.out.println("Saldo");
+        double saldo = lectura.nextDouble();
         
-        System.out.println("Global: " + Cuenta.global + '\n');
-        
-        System.out.println("Numero: " + cuenta.getNumero());
-        System.out.println("Clave: " + cuenta.getClave());
-        System.out.println("Dueno: " + cuenta.getDueno());
-        System.out.println("Saldo: " + cuenta.getSaldo());
-        
-        cuenta.setClave(3484);
-        
-        System.out.println("Clave: " + cuenta.getClave());
+        Cuenta c1 = new Cuenta(cuenta, clave, dueno, saldo);
+        c1.consignar(1000);
     }
 }
